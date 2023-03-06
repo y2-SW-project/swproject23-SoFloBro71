278cd17752f8e92e804f50dd6b40557f8f344cf5
Your Name <N00211013@iadt.ie>
2023-03-06 14:52:31 +0000

    finished view,layout and components

--- Laravel/Software-Dev/laravel-bootstrap/database/migrations/2023_03_03_100346_create_creators_table.php
+++ Laravel/Software-Dev/laravel-bootstrap/database/migrations/2023_03_03_100346_create_creators_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('games', function (Blueprint $table) {
+        Schema::create('creators', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->longText('description');
